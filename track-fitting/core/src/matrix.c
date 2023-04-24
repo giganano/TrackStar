05d9e0e426d57b312162ac87960105094389cc25
James Johnson <giganano9@gmail.com>
2023-04-24 16:26:56 -0500

    dev: matrix memory management routines, Makefile added

---
+++ track-fitting/core/src/matrix.c
@@ -0,0 +1,76 @@
+/*
+This file is part of the track-fitting package.
+Copyright (C) 2023 James W. Johnson (giganano9@gmail.com)
+License: MIT License. See LICENSE in top-level directory
+at: https://github.com/giganano/track-fitting.git.
+*/
+
+#include <stdlib.h>
+#include "matrix.h"
+
+
+/*
+Allocate memory for and return a pointer to a MATRIX object.
+Automatically initializes all matrix elements to zero.
+
+Parameters
+----------
+n_rows : ``unsigned short``
+	The number of rows in the matrix.
+n_cols : ``unsigned short``
+	The number of columns in the matrix.
+
+Returns
+-------
+mat : ``MATRIX *``
+	The newly constructed ``n_rows`` x ``n_cols`` matrix with each element
+	``mat``:math:`_{ij}` initialized to zero.
+*/
+extern MATRIX *matrix_initialize(unsigned short n_rows, unsigned short n_cols) {
+
+	unsigned short i, j;
+	MATRIX *m = (MATRIX *) malloc (sizeof(MATRIX));
+	m -> matrix = (double **) malloc (n_rows * sizeof(double *));
+	for (i = 0u; i < n_rows; i++) {
+		m -> matrix[i] = (double *) malloc (n_cols * sizeof(double));
+		for (j = 0u; j < n_cols; j++) m -> matrix[i][j] = 0.0;
+	}
+
+	/*
+	assign the number of rows and columns by modifying the stored value of the
+	memory block directly. This is the only place in the code where these
+	attributes of the MATRIX object are assigned.
+	*/
+	*((unsigned short *) &(m -> n_rows)) = n_rows;
+	*((unsigned short *) &(m -> n_cols)) = n_cols;
+	return m;
+
+}
+
+
+/*
+Free up the memory associated with a MATRIX object.
+
+Parameters
+----------
+m : ``MATRIX *``
+	The matrix to free itself.
+*/
+extern void matrix_free(MATRIX *m) {
+
+	if (m != NULL) {
+
+		if ((*m).matrix != NULL) {
+			unsigned short i;
+			for (i = 0u; i < (*m).n_rows; i++) {
+				if ((*m).matrix[i] != NULL) free(m -> matrix[i]);
+			}
+			free(m -> matrix);
+		} else {}
+
+		free(m);
+
+	} else {}
+
+}
+
---
+++ track-fitting/core/src/matrix.h
@@ -0,0 +1,93 @@
+/*
+This file is part of the track-fitting package.
+Copyright (C) 2023 James W. Johnson (giganano9@gmail.com)
+License: MIT License. See LICENSE in top-level directory
+at: https://github.com/giganano/track-fitting.git.
+*/
+
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+typedef struct matrix {
+
+	/* 
+	This struct holds the data associated with an arbitrary, 2-dimensional
+	matrix of real numbers.
+	
+	matrix : ``double **``
+		The matrix itself.
+	n_rows : ``const unsigned short``
+		The number of rows in the matrix.
+	n_cols : ``const unsigned short``
+		The number of columns in the matrix.
+	*/
+
+	double **matrix;
+	const unsigned short n_rows;
+	const unsigned short n_cols;
+
+} MATRIX;
+
+typedef struct covariance_matrix {
+
+	/*
+	This struct holds the data associated with a covariance matrix and is a
+	pointer type compatible with MATRIX. In practice, n_rows and n_cols
+	must be equal, and track-fitting's python API enforces this.
+	
+	matrix : ``double **``
+		The covariance matrix itself.
+	n_rows : ``const unsigned short``
+		The number of rows in the matrix.
+	n_cols : ``const unsigned short``
+		The number of columns in the matrix. Equal to n_rows for this
+		particular matrix type.
+	inv : ``MATRIX *``
+		The inverse of this particular covariance matrix.
+	*/
+
+	double **matrix;
+	const unsigned short n_rows;
+	const unsigned short n_cols;
+	MATRIX *inv;
+
+} COVARIANCE_MATRIX;
+
+/*
+Allocate memory for and return a pointer to a MATRIX object.
+Automatically initializes all matrix elements to zero.
+
+Parameters
+----------
+n_rows : ``unsigned short``
+	The number of rows in the matrix.
+n_cols : ``unsigned short``
+	The number of columns in the matrix.
+
+Returns
+-------
+mat : ``MATRIX *``
+	The newly constructed ``n_rows`` x ``n_cols`` matrix with each element
+	``mat``:math:`_{ij}` initialized to zero.
+*/
+extern MATRIX *matrix_initialize(unsigned short n_rows, unsigned short n_cols);
+
+/*
+Free up the memory associated with a MATRIX object.
+
+Parameters
+----------
+m : ``MATRIX *``
+	The matrix to free itself.
+*/
+extern void matrix_free(MATRIX *m);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* MATRIX_H */
