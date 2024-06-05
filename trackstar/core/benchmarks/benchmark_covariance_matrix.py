54032e5a0ec3c4a52804fd081cc4441b4ab48764
James Johnson <giganano9@gmail.com>
2024-06-05 10:39:03 -0600

    bench: covariance matrix benchmarks

---
+++ trackstar/core/benchmarks/benchmark_covariance_matrix.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+#
+# This file is part of the TrackStar package.
+# Copyright (C) 2023 James W. Johnson (giganano9@gmail.com)
+# License: MIT License. See LICENSE in top-level directory
+# at: https://github.com/giganano/trackstar.git.
+
+from trackstar.benchmarks import benchmark
+from trackstar.core.tests.test_datum import _TEST_DATUM_
+from trackstar import datum
+
+@benchmark(args = [[_TEST_DATUM_]])
+class benchmark_covariance_matrix:
+
+	def __init__(self, vector):
+		self.datum = datum(vector)
+		self.cov = self.datum.cov
+
+	@benchmark(tolerance = 3e-6)
+	def benchmark_getitem(self):
+		return self.cov["x", "y"]
+
+	@benchmark(tolerance = 5e-6)
+	def benchmark_setitem(self):
+		self.cov["x", "y"] = 1
