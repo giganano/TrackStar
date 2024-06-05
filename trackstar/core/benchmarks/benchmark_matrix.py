0895d9555a6209234a3a8f54fb5965f2b7db3a8a
James Johnson <giganano9@gmail.com>
2024-06-05 10:00:51 -0600

    bench: rounded out matrix benchmarks

--- trackstar/core/benchmarks/benchmark_matrix.py
+++ trackstar/core/benchmarks/benchmark_matrix.py
@@ -9,7 +9,6 @@ from trackstar.benchmarks import benchmark
 from trackstar import matrix
 import numpy as np
 
-
 @benchmark(
 	args = [[2], [3], [4], [5], [6], [7], [8], [9], [10]],
 	repeat = 1000)
@@ -21,23 +20,35 @@ class square_matrix:
 			for j in range(size):
 				self.mat[i, j] = self.mat[j, i] = np.random.random()
 
-	@benchmark(repeat = 100, tolerance = 1e-6)
+	@benchmark(tolerance = 1.5e-6)
+	def benchmark_getitem(self):
+		return self.mat[0, 0]
+
+	@benchmark(tolerance = 1.5e-6)
+	def benchmark_setitem(self):
+		self.mat[0, 0] = 1
+
+	@benchmark(tolerance = 2.5e-5)
+	def benchmark_add(self):
+		return self.mat + self.mat
+
+	@benchmark(tolerance = 2.5e-5)
+	def benchmark_subtract(self):
+		return self.mat - self.mat
+
+	@benchmark(tolerance = 2.5e-5)
+	def benchmark_multiply(self):
+		return self.mat * self.mat
+
+	@benchmark(tolerance = 1e-6)
 	def benchmark_determinant(self):
 		return self.mat.determinant()
 
-	@benchmark(tolerance = 1.5e-4)
+	@benchmark(tolerance = 1.5e-4, repeat = 100)
 	def benchmark_invert(self):
 		return self.mat.invert()
 
+	@benchmark(tolerance = 2.5e-5)
+	def benchmark_transpose(self):
+		return self.mat.transpose()
 
-@benchmark(
-	args = [[2], [3], [4], [5], [6], [7], [8], [9], [10]],
-	repeat = 1000)
-def benchmark_foo(x):
-	return x + 12
-
-@benchmark
-def bar_benchmark():
-	return "hello"
-
-		
